f43b008471bf1dc2b93cd9fba95e44df3170da84
Cody <cvenditto.dev@gmail.com>
2026-02-07 07:49:42 -0500

    Add preact Greeting component and dark theme toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://dancing-otter-ad592e.netlify.app",
+  site: "https://dancing-otter-ad592e.netlify.app",
+  integrations: [preact()],
 });
